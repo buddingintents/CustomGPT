0d40ee40ac77ffa93bc76f49443e39857c7079fd
BuddingIntents <79275023+buddingintents@users.noreply.github.com>
2025-01-30 15:17:40 +0530

    Update CustomGPT.py

--- CustomGPT.py
+++ CustomGPT.py
@@ -20,14 +20,6 @@ except LookupError:
     nltk.download('wordnet')
     nltk.download('stopwords')
     nltk.download
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
         
 # Text preprocessing functions
 lemmatizer = WordNetLemmatizer()
